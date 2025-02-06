b57364f07c8cacef0e52446b7163aa3fb2aecc4c
Simo Haatainen <simo.haatainen@gmail.com>
2025-02-06 21:18:53 +0200

    Add pinia for state handling

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // src/main.js
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.use(store)
 app.mount('#app')
 
