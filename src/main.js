f6492e19ccc297a5f9c2687e85b53735a2fcf132
Simo Haatainen <simo.haatainen@gmail.com>
2025-02-08 15:35:42 +0200

    Remove unused files and packages

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
 const pinia = createPinia()
 const app = createApp(App)
-app.use(router)
 app.use(pinia)
-app.use(store)
 app.mount('#app')
 
